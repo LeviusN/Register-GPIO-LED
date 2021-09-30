a143abecbef03db432553e52db3e2d29911e7708
LeviusN <91337423+LeviusN@users.noreply.github.com>
2021-09-30 21:40:32 +0200

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -47,17 +47,35 @@ int main(void)
 
   /* Enable clock for GPIO port A*/
 
-	//type your code for GPIOA clock enable here:
+*((volatile uint32_t *) (uint32_t)(0x40021000 + 0x00000014U)) |= (uint32_t)(1 << 18);//type your code for GPIOA clock enable here:
 
 
   /* GPIOA pin 3 and 4 setup */
 
-	//type your code for GPIOA pins setup here:
+  /*GPIO MODER register*/
+  //Set mode for pin 3
+  *((volatile uint32_t *)((uint32_t)0x48000400)) &= ~(uint32_t)(0x3 << 6);
+  *((volatile uint32_t *)((uint32_t)0x48000400)) |= (uint32_t)(1 << 6);
+  //Set mode for pin 6
+  *((volatile uint32_t *)((uint32_t)0x48000400)) &= ~(uint32_t)(0x3 << 12);
+
+  /*GPIO OTYPER register*/
+  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x04U))) &= ~(1 << 3);
+
+  /*GPIO OSPEEDR register*/
+  //Set Low speed for GPIOB pin 3
+  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x08U))) &= ~(0x3 << 6);
+
+  /*GPIO PUPDR register, reset*/
+  //Set pull up for GPIOA pin 3 (input)
+  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x0CU))) |= (1 << 12);
+  //Set no pull for GPIOA pin 4
+  *((volatile uint32_t *)((uint32_t)(0x48000400 + 0x0CU))) &= ~(0x3 << 6);
 
 
   while (1)
   {
-	  if(BUTTON_GET_STATE)
+	  if(!(*((volatile uint32_t *)((uint32_t)(0x48000400 + 0x10U))) & (1 << 6)))
 	  {
 		  // 0.25s delay
 		  LL_mDelay(250);
